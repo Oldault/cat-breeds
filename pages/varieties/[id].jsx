2da2f61d3072269cb8d770ade1bb2b8a986c03f8
Simon <Monkeymandeveloper@protonmail.com>
2022-09-15 20:31:00 +0200

    Improved cat page design

--- pages/varieties/[id].jsx
+++ pages/varieties/[id].jsx
@@ -3,7 +3,8 @@ import * as React from "react";
 import { useRouter } from "next/router";
 import { checkCustomRoutes } from "next/dist/lib/load-custom-routes";
 import Image from "next/image";
-import styles from "../../styles/Id.module.css"
+import styles from "../../styles/Id.module.css";
+import Head from "next/head";
 
 function ID() {
   const router = useRouter();
@@ -32,7 +33,6 @@ function ID() {
     check();
   }, [getData, id]);
 
-
   console.log(data);
 
   if (data === undefined) return null;
@@ -40,10 +40,61 @@ function ID() {
   const catData = data.breeds[0];
   return (
     <div className={styles.box}>
+      <Head>
+        <title>{catData.name}</title>
+      </Head>
       <div className={styles.content}>
-        <img src={`https://cdn2.thecatapi.com/images/${catData.reference_image_id}.jpg`} alt={catData.id} width={500} />
-        <h1>{catData.name}</h1>
-        <p>{catData.description}</p>
+        <div className={styles.left_side}>
+          <div className={styles.left_side_text}>
+            <div className={styles.top_text}>
+              <div className={styles.title}>
+                <h1>{catData.name}</h1>
+                <div className={styles.black_line}></div>
+              </div>
+              <div className={styles.gen_info}>
+                <div className={styles.desc}>
+                  <p>{catData.description}</p>
+                </div>
+                <div className={styles.details}>
+                  <p>
+                    Originiated from : <em>{catData.origin}</em>.
+                  </p>
+                  <p>
+                    Average life span is around <em>{catData.life_span}</em>{" "}
+                    years.
+                  </p>
+                  <p>
+                    Inteligence is <em>{catData.intelligence} / 5</em>!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.bottom_text}>
+              <h6>
+                <em>
+                  <a
+                    href={catData.vcahospitals_url}
+                    target="_blank"
+                    rel="noreferrer"
+                  >
+                    Read more here
+                  </a>
+                </em>
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className={styles.right_side}>
+          <img
+            src={`https://cdn2.thecatapi.com/images/${catData.reference_image_id}.jpg`}
+            alt={catData.id}
+            width={500}
+          />
+          <div className={styles.right_side_text}>
+            <h4>Temperments are:</h4>
+            <p>{catData.temperament}</p>
+          </div>
+        </div>
       </div>
     </div>
   );
